20eba641878f5a333390d256b155d29abacbfbb2
Locofel95 <150103087+Locofel95@users.noreply.github.com>
2023-11-06 22:31:46 -0500

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,139 @@
+// script.js
+
+// Selecciona la tarjeta por su ID
+
+const ensaladaCard = document.getElementById('ensalada');
+const BruschettaCard = document.getElementById('Bruschetta');
+const CalamaresCard = document.getElementById('Calamares');
+const LasañaCard = document.getElementById('Lasaña');
+const PechugaCard = document.getElementById('Pechuga');
+const RisottoCard = document.getElementById('Risotto');
+const TiramisúCard = document.getElementById('Tiramisú');
+const CheesecakeCard = document.getElementById('Cheesecake');
+const HeladoCard = document.getElementById('Helado');
+const MargaritaCard = document.getElementById('Margarita');
+const VinoCard = document.getElementById('Vino');
+const CaféCard = document.getElementById('Café');
+
+// Añade un evento cuando el mouse entra en la tarjeta
+ensaladaCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    ensaladaCard.style.backgroundColor = '#ffcccb';
+});
+
+BruschettaCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    BruschettaCard.style.backgroundColor = '#ffcccb';
+});
+
+CalamaresCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    CalamaresCard.style.backgroundColor = '#ffcccb';
+});
+
+LasañaCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    LasañaCard.style.backgroundColor = '#ffcccb';
+});
+
+PechugaCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    PechugaCard.style.backgroundColor = '#ffcccb';
+});
+
+RisottoCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    RisottoCard.style.backgroundColor = '#ffcccb';
+});
+
+TiramisúCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    TiramisúCard.style.backgroundColor = '#ffcccb';
+});
+
+CheesecakeCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    CheesecakeCard.style.backgroundColor = '#ffcccb';
+});
+
+HeladoCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    HeladoCard.style.backgroundColor = '#ffcccb';
+});
+
+MargaritaCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    MargaritaCard.style.backgroundColor = '#ffcccb';
+});
+
+VinoCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    VinoCard.style.backgroundColor = '#ffcccb';
+});
+
+CaféCard.addEventListener('mouseover', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    CaféCard.style.backgroundColor = '#ffcccb';
+});
+
+
+// Añade un evento cuando el mouse sale de la tarjeta
+ensaladaCard.addEventListener('mouseout', function() {
+    // Restaura el color de fondo al quitar el mouse
+    ensaladaCard.style.backgroundColor = '#f1f1f1';
+});
+
+BruschettaCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    BruschettaCard.style.backgroundColor = '#f1f1f1';
+});
+
+CalamaresCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    CalamaresCard.style.backgroundColor = '#f1f1f1';
+});
+
+LasañaCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    LasañaCard.style.backgroundColor = '#f1f1f1';
+});
+
+PechugaCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    PechugaCard.style.backgroundColor = '#f1f1f1';
+});
+
+RisottoCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    RisottoCard.style.backgroundColor = '#f1f1f1';
+});
+
+TiramisúCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    TiramisúCard.style.backgroundColor = '#f1f1f1';
+});
+
+CheesecakeCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    CheesecakeCard.style.backgroundColor = '#f1f1f1';
+});
+
+HeladoCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    HeladoCard.style.backgroundColor = '#f1f1f1';
+});
+
+MargaritaCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    MargaritaCard.style.backgroundColor = '#f1f1f1';
+});
+
+VinoCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    VinoCard.style.backgroundColor = '#f1f1f1';
+});
+
+CaféCard.addEventListener('mouseout', function() {
+    // Cambia el color de fondo al pasar el mouse por encima
+    CaféCard.style.backgroundColor = '#f1f1f1';
+});
